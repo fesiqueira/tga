2393523fbbdb77afd351c8d316f7a7ed42282968
Felipe Siqueira Pinheiro <fesiqueirapinheiro@gmail.com>
2022-08-18 17:29:31 -0300

    Add basic Read implementation
    Only capable of reading the file header

---
+++ tga_test.go
@@ -0,0 +1,100 @@
+package tga
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+	testCases := []struct {
+		filename string
+		expected Header
+	}{
+		{
+			filename: "./assets/test.tga",
+			expected: Header{
+				IDLength:        0,
+				ColorMapType:    0,
+				DataTypeCode:    UncompressedRGBImage,
+				ColorMapOrigin:  0,
+				ColorMapLength:  0,
+				ColorMapDepth:   0,
+				XOrigin:         0,
+				YOrigin:         0,
+				Width:           256,
+				Height:          256,
+				BitsPerPixel:    32,
+				ImageDescriptor: 8,
+			},
+		},
+		{
+			filename: "./assets/test2.tga",
+			expected: Header{
+				IDLength:        0,
+				ColorMapType:    0,
+				DataTypeCode:    UncompressedRGBImage,
+				ColorMapOrigin:  0,
+				ColorMapLength:  0,
+				ColorMapDepth:   0,
+				XOrigin:         0,
+				YOrigin:         0,
+				Width:           1280,
+				Height:          853,
+				BitsPerPixel:    24,
+				ImageDescriptor: 0,
+			},
+		},
+		{
+			filename: "./assets/flag_t16.tga",
+			expected: Header{
+				IDLength:        0,
+				ColorMapType:    0,
+				DataTypeCode:    UncompressedRGBImage,
+				ColorMapOrigin:  0,
+				ColorMapLength:  0,
+				ColorMapDepth:   0,
+				XOrigin:         0,
+				YOrigin:         0,
+				Width:           124,
+				Height:          124,
+				BitsPerPixel:    16,
+				ImageDescriptor: 32,
+			},
+		},
+		{
+			filename: "./assets/xing_t24.tga",
+			expected: Header{
+				IDLength:        0,
+				ColorMapType:    0,
+				DataTypeCode:    UncompressedRGBImage,
+				ColorMapOrigin:  0,
+				ColorMapLength:  0,
+				ColorMapDepth:   0,
+				XOrigin:         0,
+				YOrigin:         0,
+				Width:           240,
+				Height:          164,
+				BitsPerPixel:    24,
+				ImageDescriptor: 32,
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		f, err := os.Open(tc.filename)
+		if err != nil {
+			t.Fatalf("test %d: failed to open test file: %v", i+1, err)
+		}
+		defer f.Close()
+
+		header, err := Read(f)
+		if err != nil {
+			t.Errorf("failed to read test file: %v", err)
+		}
+
+		if !reflect.DeepEqual(tc.expected, header) {
+			t.Errorf("\nexpected %+v, \nbut got  %+v", tc.expected, header)
+		}
+	}
+}
